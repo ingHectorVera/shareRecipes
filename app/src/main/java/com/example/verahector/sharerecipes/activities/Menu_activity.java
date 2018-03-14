8597af6ea98ef4331acfc12f3cf18a397d7b108e
verahector <verahector26@gmail.com>
2018-03-13 21:36:03 -0600

    login user/password.

--- app/src/main/java/com/example/verahector/sharerecipes/activities/Menu_activity.java
+++ app/src/main/java/com/example/verahector/sharerecipes/activities/Menu_activity.java
@@ -1,13 +1,13 @@
-package com.example.verahector.sharerecipes;
+package com.example.verahector.sharerecipes.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Menu_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu_activity);
     }
 }
